5f885305327d968e773685eb8fa1b57d9ed9dcf4
gilart729157 <claudianoart@gmail.com>
2026-07-13 10:58:15 -0300

    Refactor Firebase database export in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,64 @@
+// =====================================
+// Firebase - Rifa GilFest
+// =====================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import {
-    getDatabase
+    getDatabase,
+    ref,
+    set,
+    get,
+    child,
+    update,
+    remove,
+    onValue
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
+// Configuração do Firebase
+
 const firebaseConfig = {
+
     apiKey: "AIzaSyDFFi2nbKHYkBDgThN6MlCy-jw5Se7eYfg",
+
     authDomain: "rifa-1bc12.firebaseapp.com",
+
     databaseURL: "https://rifa-1bc12-default-rtdb.firebaseio.com",
+
     projectId: "rifa-1bc12",
+
     storageBucket: "rifa-1bc12.firebasestorage.app",
+
     messagingSenderId: "331108957670",
+
     appId: "1:331108957670:web:1875f8481555beddbb6a27"
+
 };
+
+// Inicializa
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+const db = getDatabase(app);
+
+// Exporta para o script.js
+
+export {
+
+    db,
+
+    ref,
+
+    set,
+
+    get,
+
+    child,
+
+    update,
+
+    remove,
+
+    onValue
+
+};
